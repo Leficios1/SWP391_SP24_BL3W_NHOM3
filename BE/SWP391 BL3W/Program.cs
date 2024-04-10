a6a0077817a1ecc44bc9adab798a0df24c169694
Leficios <khoatlse173181@fpt.edu.vn>
2024-04-10 16:20:20 +0700

    Database and UserAPI

--- BE/SWP391 BL3W/Program.cs
+++ BE/SWP391 BL3W/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using SWP391_BL3W.Database;
+using SWP391_BL3W.Repository;
+using SWP391_BL3W.Repository.Interface;
+using SWP391_BL3W.Services;
+using SWP391_BL3W.Services.Interface;
+
 namespace SWP391_BL3W
 {
     public class Program
@@ -14,6 +21,13 @@ namespace SWP391_BL3W
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddCors(o => o.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+            builder.Services.AddDbContext<SWPContext>(options => {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ElectricStore"));
+            });
+            builder.Services.AddAutoMapper(typeof(Program));
+            //Register Service And Repo
+            builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            builder.Services.AddScoped<IUserService, UserService>();
 
             var app = builder.Build();
 
---
+++ BE/SWP391 BL3W/Services/Interface/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWP391_BL3W.DTO;
+
+namespace SWP391_BL3W.Services.Interface
+{
+    public interface IUserService
+    {
+        Task<IActionResult> EditAvater(string link, int userId);
+        Task<StatusResponse<UserResponseDto>> CreateUser(CreateUserDTO user);
+
+        Task<IEnumerable<UserResponseDto>> GetUsers(int? pageNumber, int? pageSize);
+
+        Task<StatusResponse<UserResponseDto>> GetUser(int id);
+
+        Task<StatusResponse<UserResponseDto>> UpdateUser(UpdateUserDTO user);
+    }
+}
