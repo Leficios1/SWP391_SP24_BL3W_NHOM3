d0eb7fcca1e6d287aab5fc8d55b89de91b4b6aca
nguyễn khương <146073034+nguyen-khuongca@users.noreply.github.com>
2024-04-20 18:35:33 +0700

    update categoryblog and blog

---
+++ BE/SWP391 BL3W/Migrations/20240420091422_UpdateDBandSeederImages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SWP391_BL3W.Migrations
+{
+    public partial class UpdateDBandSeederImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateTime(2024, 4, 20, 16, 14, 22, 176, DateTimeKind.Local).AddTicks(4641));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateTime(2024, 4, 20, 15, 57, 58, 795, DateTimeKind.Local).AddTicks(6215));
+        }
+    }
+}
